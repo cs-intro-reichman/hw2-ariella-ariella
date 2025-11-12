47d653ecc895f7a8d8c54b3b5b32784c9a040e0b
Ariella Stefansky <ariella.stefansky@post.runi.ac.il>
2025-11-12 15:41:06 +0200

    Completed assignment

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,17 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+		double sum = 0.0;
+		double denominator = 1.0;
+	    for (int termIndex = 0; termIndex < Integer.parseInt(args[0]); termIndex++) {
+			if (termIndex % 2 == 0) {
+				sum += (1.0/denominator);
+			} else {
+				sum -= (1.0/denominator);
+			}
+			denominator += 2;
+		}
+		System.out.println("pi according to Java: " + Math.PI); 
+		System.out.println("pi, approximated:     " + (4 * sum));
 	}
 }
--- Cheers.java
+++ Cheers.java
@@ -8,6 +8,30 @@ Great work boaz!
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	    String stringOfLetters = args[0];
+            String lowerCaseLetterBank = "abcdefghijklmnopqrstuvwxyz";
+            String upperCaseLetterBank = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            for (int index = 0; index < stringOfLetters.length(); index ++) {
+                int indexOfLower = lowerCaseLetterBank.indexOf(stringOfLetters.charAt(index));
+                if (indexOfLower == -1) {
+                        break;
+                } else {
+                        String upperCaseChar = upperCaseLetterBank.charAt(indexOfLower);
+                        stringOfLetters = stringOfLetters.substring(0, index) + upperCaseChar + stringOfLetters.substring(index +1);
+                }
+            }
+            String anLetters = "AEFHILMNORSX";
+            for (int letterIndex = 0; letterIndex < stringOfLetters.length(); letterIndex++) {
+                String curLetter = stringOfLetters.charAt(letterIndex);
+                if (anLetters.indexOf(curLetter) == -1) {
+                        System.out.println("Give me a " + curLetter + ": " + curLetter + "!");
+                } else {
+                        System.out.println("Give me an " + curLetter + ": " + curLetter + "!");
+                }
+            }
+            System.out.println("What does that spell?");
+            for( int i = 0; i < Integer.parseInt(args[1]); i++) {
+                System.out.println(stringOfLetters + "!!!");
+            }
         }
 }
